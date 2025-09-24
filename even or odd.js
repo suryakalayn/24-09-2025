2b503bc273b6bea57b8a1a375098cb0493055699
SuryaKalyan <suryakalyanpasupuleti@gmail.com>
2025-09-24 08:38:03 +0530

    JS Files

---
+++ even or odd.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(checkEvenOdd(4)); 
+console.log(checkEvenOdd(7)); 
+console.log(factorial(5));
---
+++ subtraction.js
@@ -0,0 +1,4 @@
+let a=10;
+let b=20;
+let sub=b-a;
+console.log("the sub is :"+sub)
